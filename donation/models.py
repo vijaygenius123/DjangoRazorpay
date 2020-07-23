bc7af16ee58d9447644b43aabb5c76399a32fdfd
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-23 19:34:41 +0530

    Basic Razorpay Integration Done

--- donation/models.py
+++ donation/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Donation(models.Model):
     name = models.CharField(max_length=50)
     amount = models.IntegerField()
+    payment_id = models.CharField(max_length=50,blank=True,null=True)
+    payment_captured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name + ": " + str(self.amount)
--- donation/views.py
+++ donation/views.py
@@ -12,7 +12,30 @@ def home(request):
     if request.method == 'POST':
         form = DonationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return render(request,'checkout.html')
+            donation = form.save()
+            print(donation.id,donation.name,str(donation.amount))
+            data = {
+                'amount':donation.amount * 100,
+                'currency': 'INR',
+                'receipt': str(donation.id),
+                'payment_capture':'1',
+                'notes': {
+                    'name': donation.name
+                }
+            }
+            resp = client.order.create(data=data)
+            donation.payment_id = resp['id']
+            donation.save()
+
+            context = {
+                'id': resp['id'],
+                'amount': resp['amount'] * 100,
+                'name': donation.name
+            }
+            return render(request,'checkout.html',context=context)
     return render(request,'home.html')
+
+def success(request):
+    if request.method == 'POST':
+        return render(request,'success.html')
 
