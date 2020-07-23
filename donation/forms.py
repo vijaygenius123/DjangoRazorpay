c4e835600320f7993b97355eb5eb7f3d4ec932de
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-23 19:01:45 +0530

    Added Form For Donation & Checkout Page

---
+++ donation/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+from .models import Donation
+
+class DonationForm(forms.ModelForm):
+    class Meta:
+        model = Donation
+        fields = '__all__'
--- donation/views.py
+++ donation/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from .forms import DonationForm
+
+import razorpay
+
+
+client = razorpay.Client(auth=("rzp_test_9GtISjPbi1bNeB", "TXyNCg77zThOEcQ8g9p5tQDl"))
+
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = DonationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'checkout.html')
     return render(request,'home.html')
+
